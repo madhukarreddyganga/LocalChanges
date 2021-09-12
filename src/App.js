ab6858b1bb3fe9ae504dd499def4cc2696b1cea7
madhukarreddyganga <mk022104@gmail.com>
2021-09-11 20:31:17 -0400

    changes1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App=() =>{
   var a= 20;
   return (
     <div className="App">
@@ -9,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1> How are You got money {a} rupees from my parent</h1>
         <h1> How are You got money {a} rupees from my parent</h1>
+        <h1> Where are you going {a} rupees from my parent</h1>
+        <h1> I got {a} rupees from my father</h1>
+        
       </header>
     </div>
   );
