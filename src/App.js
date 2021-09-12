6d3165d14580012d2d4480f00c7a086eb1556850
madhukarreddyganga <mk022104@gmail.com>
2021-09-11 20:02:17 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  var a= 20;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> How are You got money {a} rupees from my parent</h1>
+        <h1> How are You got money {a} rupees from my parent</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
